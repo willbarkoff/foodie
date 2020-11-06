09f0f64c801e70b705d72fc75d6ecd8229f3d39d
William Barkoff <william@barkoffusa.com>
2020-11-06 00:25:24 -0500

    Improve greetings

--- src/components/eateries/Greeting.tsx
+++ src/components/eateries/Greeting.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 function getGreeting(): string {
 	const time = new Date();
-	if (time.getHours() > 12 && time.getHours() < 4) {
+	if (time.getHours() < 4) {
 		return "Working late?";
 	} else if (time.getHours() < 11) {
 		return "Good morning!";
