fd0621a1bde6c7064b5c2e117a83a0ee836135ae
William Barkoff <william@barkoffusa.com>
2021-02-03 19:15:59 -0500

    Add sorting options

--- src/components/eateries/EateriesList.tsx
+++ src/components/eateries/EateriesList.tsx
@@ -3,6 +3,8 @@ import "@creativebulma/bulma-tooltip/dist/bulma-tooltip.min.css";
 import * as Eateries from "../../apis/eateries";
 import NoResults from "../ui/NoResults";
 import Eatery from "./Eatery";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 interface EateriesListProps {
 	eateries: Eateries.Eatery[];
 }
@@ -10,6 +12,25 @@ interface EateriesListProps {
 const createLocationFilter = (loc: string) => {
 	return (e: Eateries.Eatery) => e.campusArea.descrshort == loc;
 };
+
+const deg2rad = (deg: number) => {
+	return deg * (Math.PI / 180)
+}
+
+// thanks https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+const haversine = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+	var dLon = deg2rad(lon2 - lon1);
+	var a =
+		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+		Math.sin(dLon / 2) * Math.sin(dLon / 2)
+		;
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c; // Distance in km
+	return d;
+}
 
 const filters: Record<string, (e: Eateries.Eatery) => boolean> = {
 	"north": createLocationFilter("North"),
@@ -26,6 +47,9 @@ const EateriesList: React.FC<EateriesListProps> = ({ eateries }) => {
 	const [currentFilterChain, setCurrentFilterChain] = React.useState([] as string[]);
 	const [locationFilterValue, setLocationFilterValue] = React.useState("");
 	const [stateFilterValue, setStateFilterValue] = React.useState("");
+	const [sort, updateSort] = React.useState("openclose");
+	const [geolocation, updateGeolocation] = React.useState(null as GeolocationPosition | null);
+	const [geoErr, updateGeoErr] = React.useState(null as GeolocationPositionError | null);
 
 	const filtered = eateries.filter(currentFilterChain.map(f => filters[f]).reduce((acc, cv) => {
 		return (e: Eateries.Eatery) => {
@@ -36,7 +60,35 @@ const EateriesList: React.FC<EateriesListProps> = ({ eateries }) => {
 		};
 	}, () => true));
 
+	if (sort == "closest" && !geolocation && !geoErr) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			updateGeolocation(position);
+			updateGeoErr(null);
+		}, (err) => {
+			updateGeoErr(err);
+		});
+	}
+
+	const sorts: Record<string, (a: Eateries.Eatery, b: Eateries.Eatery) => number> = {
+		"alphebetical": (a, b) => {
+			if (a.name.toLowerCase() < b.name.toLowerCase()) {
+				return -1;
+			} else if (b.name.toLowerCase() < a.name.toLowerCase()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		},
+		"openclose": (a, b) => (Eateries.isOpen(a) == Eateries.isOpen(b) ? 0 : Eateries.isOpen(a) ? -1 : 1),
+		"closest": (a, b) => haversine(a.coordinates.latitude, a.coordinates.longitude, geolocation?.coords.latitude, geolocation?.coords.longitude) - haversine(b.coordinates.latitude, b.coordinates.longitude, geolocation?.coords.latitude, geolocation?.coords.longitude),
+	};
+
+
+	const sorted = filtered.sort(sorts[sort]);
+
 	const updateLocationFilter = (f?: string) => {
+		setLocationFilterValue(f || "");
+
 		const filters = currentFilterChain;
 		locationFilters.forEach(filter => {
 			const index = filters.indexOf(filter);
@@ -49,10 +101,11 @@ const EateriesList: React.FC<EateriesListProps> = ({ eateries }) => {
 			filters.push(f);
 		}
 		setCurrentFilterChain(filters);
-		setLocationFilterValue(f || "");
 	};
 
 	const updateStateFilter = (f?: string) => {
+		setStateFilterValue(f || "");
+
 		const filters = currentFilterChain;
 		stateFilters.forEach(filter => {
 			const index = filters.indexOf(filter);
@@ -66,7 +119,6 @@ const EateriesList: React.FC<EateriesListProps> = ({ eateries }) => {
 		}
 
 		setCurrentFilterChain(filters);
-		setStateFilterValue(f || "");
 	};
 
 	return <div>
@@ -100,16 +152,18 @@ const EateriesList: React.FC<EateriesListProps> = ({ eateries }) => {
 				</select>
 			</div> */}
 			.
-			{/* List{" "}
+			List{" "}
 			<div className="select is-medium">
-				<select>
+				<select onChange={e => updateSort(e.target.value)} value={sort}>
 					<option value="openclose">open eateries first</option>
+					{"geolocation" in navigator && <option value="closest">closest eateries first</option>}
 					<option value="alphebetical">in alphebetical order</option>
 				</select>
-			</div>. */}
+			</div>.
 		</h3>
-		{filtered.length == 0 && <NoResults />}
-		{filtered.map((eatery, i) => <Eatery key={i} eatery={eatery} />)}
+		{geoErr && <div className="notification is-warning"><FontAwesomeIcon icon={faExclamationTriangle} />{" " + geoErr.message}</div>}
+		{sorted.length == 0 && <NoResults />}
+		{sorted.map((eatery, i) => <Eatery key={i} eatery={eatery} />)}
 	</div>;
 };
 
