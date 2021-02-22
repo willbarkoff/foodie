d4066a6c688ed463859b475378e7779d1acbe467
William Barkoff <william@barkoffusa.com>
2021-02-22 12:43:51 -0500

    Fix code style

--- src/components/eateries/EateriesList.tsx
+++ src/components/eateries/EateriesList.tsx
@@ -14,23 +14,23 @@ const createLocationFilter = (loc: string) => {
 };
 
 const deg2rad = (deg: number) => {
-	return deg * (Math.PI / 180)
-}
+	return deg * (Math.PI / 180);
+};
 
 // thanks https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
 const haversine = (lat1: number, lon1: number, lat2: number, lon2: number) => {
-	var R = 6371; // Radius of the earth in km
-	var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-	var dLon = deg2rad(lon2 - lon1);
-	var a =
+	const R = 6371; // Radius of the earth in km
+	const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+	const dLon = deg2rad(lon2 - lon1);
+	const a =
 		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
 		Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
 		Math.sin(dLon / 2) * Math.sin(dLon / 2)
 		;
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	var d = R * c; // Distance in km
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	const d = R * c; // Distance in km
 	return d;
-}
+};
 
 const filters: Record<string, (e: Eateries.Eatery) => boolean> = {
 	"north": createLocationFilter("North"),
